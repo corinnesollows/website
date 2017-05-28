d1baaa05de74c62a520648590af46ff88f61fe83
corinnesollows <corinne.sollows@gmail.com>
2017-05-28 14:30:14 -0400

    adding files
    Adding all website files

---
+++ JS/homenewscroll.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            var id = this.hash;
+            var url = document.URL,
+            shortUrl = url.substring(0, url.lastIndexOf("/"));
+            window.location.href = shortUrl + '/Projects.html' + id;
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
---
+++ JS/top.js
@@ -0,0 +1,16 @@
+﻿
+$(function () {
+    $(".backtotop").hide();
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {
+        $('.backtotop').fadeIn(400);
+    } else {
+        $('.backtotop').fadeOut(400);
+    }
+});
+
+$("#backtotop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+});
